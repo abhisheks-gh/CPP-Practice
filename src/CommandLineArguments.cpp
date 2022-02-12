167feaacb0fe245ca067c8471b83d62dcf1be3ea
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-02-12 21:33:38 +0530

    Update CommandLineArguments.cpp

--- src/CommandLineArguments.cpp
+++ src/CommandLineArguments.cpp
@@ -1,28 +1,30 @@
-//Command line arguments
-#include<iostream>
-using namespace std;
-int main(int argc, char*argv[]) //Arguments can be pass directly through main() also.
+// Command line arguments
 
-// We can use any other name insteada argc & argv. But, commonly these two names are used.
-{ 
-    cout<<"You have entered "<<argc /* argc--> agrument count (shows no. of commands) &
-                                       argv--> argument vector (shows list of commands) */
-    <<"arguments : "<<"\n";
-    for(int i=0;i<argc;++i){
-        cout<<argv[i]<<"\n";
+#include <iostream>
+
+
+// Arguments can be pass directly through main() also
+// We can use any other names instead of argc & argv. But, commonly these two names are used.
+
+// argc--> agrument count (shows no. of commands) 
+// argv--> argument vector (shows list of commands) 
+int main(int argc, char *argv[]) {
+    std::cout << "You have entered " << argc << " arguments... \n";
+    for (int i = 0; i < argc; ++i) {
+        std::cout << argv[i] << "\n";
         return 0;
     }
 }
 
-  //To pass an argument use command --->   .\command
-  
-  /* Syntax:-
-    int main(int argc, char*argv[]){
-        _ _ _ _
-    } 
+// To pass an argument use command --->   .\command
 
-          OR
+//  Syntax:-
+// int main(int argc, char*argv[]){
+//     _ _ _ _
+// }
 
-    int main(int argc, char**argv){
-        _ _ _ _
-    }    */
+//       OR
+
+// int main(int argc, char**argv){
+//     _ _ _ _
+// }    
