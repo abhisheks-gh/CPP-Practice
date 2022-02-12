3f65af4a14d30c24f078d3f0c469c5dfa625b1a8
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-02-12 21:09:04 +0530

    Update AccessingStaticDataMember.cpp

--- src/AccessingStaticDataMember.cpp
+++ src/AccessingStaticDataMember.cpp
@@ -3,17 +3,15 @@
 #include <iostream>
 using namespace std;
 
-class Demo
-{
-private:
-    static int x;
+class Demo {
+    private:
+        static int x;
 
-public:
-    static void fun()
-    {
-        cout << "Value of x : " << x << endl;
+    public:
+        static void fun() {
+        std::cout << "Value of x : " << x << std::endl;
     }
-
+    
     // A static data member can be acessed through the class name without using the static member function
     // (as it's a class member), here we need a scope resolution operator(SRO) :: to acess static data
     // member without using static member funtion.
@@ -22,8 +20,7 @@ public:
 // Syntax--> className::staticDataMember;
 int Demo::x = 10; 
 
-int main()
-{
+int main() {
     Demo x;
     x.fun();
     return 0;
