0642d28b33ad6c943ccc3eba8d78e683ce5a04de
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-02-18 00:27:10 +0530

    Update EnumDataType.cpp

--- src/EnumDataType.cpp
+++ src/EnumDataType.cpp
@@ -1,18 +1,21 @@
 // Program to find whether the entered day is weekday or weekend using enum datatype.
 
 #include <iostream>
-using namespace std;
 
 int main() {
-    enum Day {Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday}; 
+    enum Day {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday}; 
     enum Day today;
     int x;
-    cout<<"Please enter the day of the week(0 to 6)\n";
-    cin>>x;  
-    x=today;  
-    if(today==Sunday || today==Saturday)   
-        cout<<"Finally! It's the weekend\n";
+    
+    std::cout << "Please enter the day of the week(0 to 6)\n";
+    std::cin >> x;  
+    
+    x = today;  
+    
+    if(today == Sunday || today == Saturday)   
+        std::cout << "Finally! It's the weekend\n";
     else
-        cout<<"Week day. Get back to work!\n";
+        std::cout << "Week day. Get back to work!\n";
+    
     return 0;
 }
