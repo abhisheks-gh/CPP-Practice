2d3a0574b100e6dc9acb496f208fb98de855fd68
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-10 00:22:40 +0530

    Update volume.cpp

--- src/volume.cpp
+++ src/volume.cpp
@@ -7,16 +7,16 @@ using namespace std;
 int main() {
     int a1, r1, x1, y1;
     
-    cout<<"Enter the sides of cube : "<<endl;
-    cin>>a1;
-    cout<<"Volume of cube : "<<volume::cube(a1)<<"cube units\n";
-    cout<<"\n\n";
-    cout<<"Enter the radius of sphere : \n"<<endl;
-    cin>>r1;
-    cout<<"Volume of sphere : "<<volume::sphere(r1)<<"cube units\n";
-    cout<<"\n \n";
-    cout<<"Enter the radius and height of cylinder : \n"<<endl;
-    cin>>x1>>y1;
+    std::cout << "Enter the sides of cube : " << std::endl;
+    std::cin >> a1;
+    std::cout << "Volume of cube : " << volume::cube(a1) << "cube units\n";
+    std::cout << "\n\n";
+    std::cout << "Enter the radius of sphere : \n" << std::endl;
+    std::cin >> r1;
+    std::cout << "Volume of sphere : " << volume::sphere(r1) << "cube units\n";
+    std::cout <<"\n \n";
+    std::cout << "Enter the radius and height of cylinder : \n" << std::endl;
+    std::cin >> x1 >> y1;
     
-    cout<<"Volume of cylinder : "<<volume::cylinder(x1,y1)<<"cube units\n";
+    std::cout << "Volume of cylinder : " << volume::cylinder(x1,y1) << "cube units\n";
 }
