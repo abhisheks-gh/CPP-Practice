55563f6d4f988d4b42e839d4c248dfecbc08ea16
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-19 23:30:55 +0530

    Add files via upload

---
+++ src/student.h
@@ -0,0 +1,14 @@
+namespace students
+{
+    class stud
+    {
+    public:
+
+    char name[50];
+    int roll_no;
+    int prn;
+    int contact_no;
+    void userinput();
+    void studentinfo();
+    };
+}
