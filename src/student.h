eba593a083d746e398fdfb594c4e14f6433b52da
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-19 23:34:39 +0530

    Update student.h

--- src/student.h
+++ src/student.h
@@ -1,14 +1,12 @@
-namespace students
+namespace students 
 {
-    class stud
-    {
+    class stud {
     public:
-
-    char name[50];
-    int roll_no;
-    int prn;
-    int contact_no;
-    void userinput();
-    void studentinfo();
+        char name[50];
+        int roll_no;
+        int prn;
+        int contact_no;
+        void userinput();
+        void studentinfo();
     };
 }
