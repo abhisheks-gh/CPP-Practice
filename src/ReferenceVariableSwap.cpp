6a2eb85442193006b568c288f32e91014d2e917b
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-20 21:18:41 +0530

    Update ReferenceVariableSwap.cpp

--- src/ReferenceVariableSwap.cpp
+++ src/ReferenceVariableSwap.cpp
@@ -1,29 +1,28 @@
-/*Implement program using reference variable,class & objects
-  for swapping of two numbers. */
+// Implement program using reference variable,class & objects for swapping of two numbers.
+
 #include <iostream>
 using namespace std;
 
-class interchange
-    {
-        public:
-         void swapno(int &a , int &b ,int x)
-                {
-                    x=a;
-                    a=b;
-                    b=x;
-                }
-    };
-
-int main()
-    {
-        int a,b,x;
-        cout << "First Number = ";
-        cin >> a;
-        cout << "Second Number = ";
-        cin >> b;
-        interchange ab;
-        ab.swapno(a,b,x);
-        cout << "(After Swapping) = " << a;
-        cout << "\n(After Swapping) = " << b;
-        return 0;
+class interchange {
+public:
+    void swapno(int &a , int &b , int x) {
+        x = a;
+        a = b;
+        b = x;
     }
+};
+
+int main() {
+    int a, b, x;
+    std::cout << "First Number = ";
+    std::cin >> a;
+    std::cout << "Second Number = ";
+    std::cin >> b;
+
+    interchange ab;
+    ab.swapno(a,b,x);
+    std::cout << "(After Swapping) = " << a;
+    std::cout << "\n(After Swapping) = " << b;
+
+    return 0;
+}
