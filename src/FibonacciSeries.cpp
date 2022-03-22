235a28a252d833947bb4a5a53b6926c4bb167995
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-22 18:20:09 +0530

    Update FibonacciSeries.cpp

--- src/FibonacciSeries.cpp
+++ src/FibonacciSeries.cpp
@@ -2,17 +2,17 @@
 using namespace std;  
 
 int main() {  
-  unsigned long int a = 0, b = 1, c;
-  unsigned short int i, num;    
+ unsigned long int a = 0, b = 1, c;
+ unsigned short int i, num;    
  std::cout << "Enter num : " << std::endl;    
  std::cin >> num;    
  std::cout << a << " " << b << " " << std::endl;
- for (i=2; i < num; ++i) 
+ for (i = 2; i < num; ++i) 
  {    
   c = a + b;   
   std::cout << c << " ";    
-  a=b;   
-  b=c;    
+  a = b;   
+  b = c;    
  }    
    return 0;  
 }
