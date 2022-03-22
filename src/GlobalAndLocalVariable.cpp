ed01db8a32b1ce44f2e2f059773b7a46056d19e1
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-22 09:16:53 +0530

    Update GlobalAndLocalVariable.cpp

--- src/GlobalAndLocalVariable.cpp
+++ src/GlobalAndLocalVariable.cpp
@@ -1,17 +1,18 @@
 #include<iostream>
 using namespace std;
-int a=10; //Global Variable
-int main(){
-    int a=5;  //Local variable
+
+int a = 10; //Global Variable
+int main() {
+    int a = 5;  //Local variable
     {
-    int a=3;
-    cout<<"The  scope value of a is : "<<a;
+        int a=3;
+        std::cout << "The  scope value of a is : " << a;
     }
     {
-    cout<<"\nThe value of global a is : "<<::a; 
-    // Scope resolution operator is used to acess the value of global variable inside a scope.
+        std::cout << "\nThe value of global a is : " << ::a; 
+        // Scope resolution operator is used to acess the value of global variable inside a scope.
     
-    cout<<"\nThe value of local a is : "<<a;
+        std::cout << "\nThe value of local a is : " << a;
     }
     return 0;
 }
