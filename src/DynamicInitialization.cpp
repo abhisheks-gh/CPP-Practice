7fe3597ee2ab6627252bf8b46548b483c810069b
Abhishek Kumar <95921032+abhishekkumarXD@users.noreply.github.com>
2022-03-22 09:18:07 +0530

    Update DynamicInitialization.cpp

--- src/DynamicInitialization.cpp
+++ src/DynamicInitialization.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int a=5;                 //static initialization
-    cout<<"Please enter a value : "; //dynamic initialization 
+
+int main() {
+    int a = 5;                 // static initialization
+    std::cout << "Please enter a value : ";     // dynamic initialization 
     //In dynamic initialization, variable is initialized at the run-time by the user
-    cin>>a;
-    cout<<a; //The static value would be overwritten by the dynamic value.
+    std::cin >> a;
+    std::cout << a;     // The static value would be overwritten by the dynamic value.
+    
     return 0;
 }
